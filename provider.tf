02fedd01f78129d4fa44bd16f91610275ea316ac
vcapp <vcappuccio@gmail.com>
2024-07-06 19:37:44 +0200

    keys

--- provider.tf
+++ provider.tf
@@ -1,7 +1,11 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 provider "fakewebservices" {
   token = var.provider_token
 }
 
 variable "provider_token" {
-  type = string
+  type        = string
+  description = "The provider token for authentication"
 }
