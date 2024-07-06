411f0dc6205f651484546010661778da3a21f45b
vcapp <vcappuccio@gmail.com>
2024-07-06 19:11:56 +0200

    Key

--- provider.tf
+++ provider.tf
@@ -1,16 +1,8 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# The following variable is used to configure the provider's authentication
-# token. You don't need to provide a token on the command line to apply changes,
-# though: using the remote backend, Terraform will execute remotely in HCP Terraform
-# where your token is already securely stored in your workspace!
-
-variable "provider_token" {
-  type      = string
-  sensitive = true
-}
-
 provider "fakewebservices" {
   token = var.provider_token
+}
+
+variable "provider_token" {
+  type = string
+  default = "${{ secrets.TF_API_TOKEN }}"
 }
