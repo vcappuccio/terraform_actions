f46e70e16563b2f58c80bd8a63df73df21e48e67
vcapp <vcappuccio@gmail.com>
2024-07-06 19:53:05 +0200

    no provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,17 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+terraform {
+  required_providers {
+    fakewebservices = "~> 0.1"
+  }
+}
 
+variable "provider_token" {
+  type        = string
+  description = "The provider token for authentication"
+}
 
+provider "fakewebservices" {
+  token = var.provider_token
+}
